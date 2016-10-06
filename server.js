3c22505836ff149c739ab711498a0f9c24afd479
Cody Brunner <rockchalkwushock@icloud.com>
2016-10-06 16:02:56 -0500

    V1.2: Now serving from /build instead of /public.

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ import express from 'express';
 import socket_io from 'socket.io';
 
 let app = express();
-app.use(express.static('public'));
+app.use(express.static('build'));
 
 let server = http.Server(app);
 let io = socket_io(server);
 
-server.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 3000);
